fa8ea34c434a6986ac229119c9ee90070be664ec
DAKSH THAKUR <70480346+Just-DT21@users.noreply.github.com>
2026-09-17 02:47:50 +0530

    Add src/mobile_robot_lab/setup.py

---
+++ src/mobile_robot_lab/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = "mobile_robot_lab"
+
+setup(
+    name=package_name,
+    version="1.0.0",
+    packages=[package_name],
+    data_files=[
+        (
+            "share/ament_index/resource_index/packages",
+            ["resource/" + package_name],
+        ),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob("launch/*.launch.py"),
+        ),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    description="ROS 2 Jazzy mobile robot laboratory experiments.",
+    license="Apache-2.0",
+    entry_points={
+        "console_scripts": [
+            "publisher_node = mobile_robot_lab.publisher_node:main",
+            "subscriber_node = mobile_robot_lab.subscriber_node:main",
+        ],
+    },
+)
