89131ecb016da7f48fb06cdc6b543b2c4778ae54
DAKSH THAKUR <70480346+Just-DT21@users.noreply.github.com>
2026-09-17 02:48:00 +0530

    Add src/mobile_robot_lab/mobile_robot_lab/subscriber_node.py

---
+++ src/mobile_robot_lab/mobile_robot_lab/subscriber_node.py
@@ -0,0 +1,33 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class SubscriberNode(Node):
+    def __init__(self):
+        super().__init__("subscriber_node")
+        self.subscription = self.create_subscription(
+            String,
+            "chatter",
+            self.callback,
+            10,
+        )
+
+    def callback(self, msg):
+        self.get_logger().info(f'I heard: "{msg.data}"')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = SubscriberNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
